fc533c9c9ae863424efbf640e2c82dd9ce949520
marxsm <yeulaptrinh98@gmail.com>
2017-06-15 10:12:01 +0700

    Release 0.0.1

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
-process.stdin.resume()
-process.stdin.setEncoding('utf8')
-const util = require('util')
-
 const request = require('async-request')
+const prompt = require('prompt')
 
-const pattern = /<a href="http:\/\/azlyrics.com\/lyrics\/(.*).html" target="_blank"><b>(.*)<\/b><\/a>  by <b>(.*)<\/b><br>/g
+var pattern = /<a href="(.*)" target="_blank"><b>(.*)<\/b><\/a>  by <b>(.*)<\/b><br>/g
+var lpattern = /Sorry about that. -->([\s\S]*)<!-- MxM banner -->/g
 
 async function Request(song, page) {
 	try {
-		let url = 'http://search.azlyrics.com/search.php?q=' + song + '&w=songs&p=' + page
-		let html = await request(url)
+		var url = 'http://search.azlyrics.com/search.php?q=' + song + '&w=songs&p=' + page
+		var html = await request(url)
 		return html.body
+	}
+	catch (e) {
+		console.log(e)
+		process.exit(1)
+	}
+}
+
+async function GetLyric(url) {
+	try {
+		var html = await request(url)
+		var raw = lpattern.exec(html.body)
+		var res = raw[1].replace(/\<br\>/g," ");
+		res = res.replace(/\<i\>\[Chorus:\]\<\/i\>/g," ");
+		res = res.replace(/\<i\>\[Pre-Chorus:\]\<\/i\>/g," ");
+		console.log(res)
 	}
 	catch (e) {
 		console.log(e)
@@ -20,10 +33,18 @@ async function Request(song, page) {
 
 async function GetSongList(song, page) {
 	try {
-		html = await Request(song, page)
-		const songs = pattern.exec(html)
-		console.log(songs[1])
-		return songs
+		var html = await Request(song, page)
+		var songs = []
+		var song
+		while (song = pattern.exec(html))
+			songs.push({url : song[1], name : song[2], singer : song[3]})
+		for (var i=0; i < songs.length; i++)
+			console.log((i+1).toString() + ' - ' + songs[i].name + ' - ' + songs[i].singer)
+
+		prompt.get(['number'], (err, result) => {
+			result.number--
+			GetLyric(songs[result.number].url)
+		})
 	}
 	catch (e) {
 		console.log(e)
@@ -35,5 +56,10 @@ function clear() {
 	console.log('\033[2J')
 }
 
-let page = 1
-let song = ''
+var song, page
+
+prompt.start()
+
+prompt.get(['song', 'page'], (err, result) => {
+	GetSongList(result.song, result.page)
+})
